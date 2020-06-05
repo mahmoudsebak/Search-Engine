bde53a869a58a6da16f532533f250e8d433fe408
Mazen <mazen930@hotmail.com>
2020-06-05 09:27:07 +0200

    Add Personalized search query

--- Android/app/src/main/java/com/example/search/CustomAdapterForImageSearch.java
+++ Android/app/src/main/java/com/example/search/CustomAdapterForImageSearch.java
@@ -3,6 +3,7 @@ package com.example.search;
 import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,9 +12,22 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkResponse;
+import com.android.volley.NoConnectionError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.TimeoutError;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
 import com.squareup.picasso.Picasso;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class CustomAdapterForImageSearch extends ArrayAdapter {
     private ArrayList<ImageClass> webSitesArrayList;
@@ -42,6 +56,21 @@ public class CustomAdapterForImageSearch extends ArrayAdapter {
             @Override
             public void onClick(View v) {
                 Toast.makeText(context,"You entered Website",Toast.LENGTH_LONG).show();
+                Thread sendURL=new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        getResponse(
+                                Request.Method.GET,
+                                ULRConnection.url+"/search/user_action?"+currentWebsite.getUrl(),
+                                null,
+                                new VolleyCallback() {
+                                    @Override
+                                    public void onSuccessResponse(String response) throws JSONException {
+                                        JSONObject obj= new JSONObject(response);; }
+                                },"","1");
+                    }
+                });
+                sendURL.start();
                 Intent i = new Intent(Intent.ACTION_VIEW);
                 i.setData(Uri.parse(currentWebsite.getUrl()));
                 context.startActivity(i);
@@ -49,4 +78,66 @@ public class CustomAdapterForImageSearch extends ArrayAdapter {
         });
         return listItem;
     };
+    public void getResponse(
+            int method,
+            String url,
+            JSONObject jsonValue,
+            final VolleyCallback callback,final  String query,final String pageNumber) {
+        StringRequest stringRequest =
+                new StringRequest(
+                        method,
+                        url,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                try {
+                                    callback.onSuccessResponse(response);
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        },
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                NetworkResponse networkResponse = error.networkResponse;
+                                String errorMessage = "Unknown error";
+                                if (networkResponse == null) {
+                                    if (error.getClass().equals(TimeoutError.class)) {
+                                        errorMessage = "Request timeout";
+                                    } else if (error.getClass().equals(NoConnectionError.class)) {
+                                        errorMessage = "Failed to connect server";
+                                    }
+                                } else {
+                                    String result = new String(networkResponse.data);
+                                    try {
+                                        JSONObject response = new JSONObject(result);
+                                        String status = response.getString("status");
+                                        String message = response.getString("message");
+
+                                        Log.e("Error Status", status);
+                                        Log.e("Error Message", message);
+
+                                        if (networkResponse.statusCode == 404) {
+                                            errorMessage = "Resource not found";
+                                        } else if (networkResponse.statusCode == 500) {
+                                            errorMessage = message+" Something is getting wrong";
+                                        }
+                                    } catch (JSONException e) {
+                                        e.printStackTrace();
+                                    }
+                                }
+                                Log.i("Error", errorMessage);
+                                error.printStackTrace();
+                            }
+                        }) {
+                    @Override
+                    protected Map<String, String> getParams() throws AuthFailureError {
+                        Map<String, String> params = new HashMap<>();
+                        params.put("suggestion", query);
+                        return params;
+                    }
+                };
+        VolleySingleton.getInstance(getContext()).addToRequestQueue(stringRequest);
+    }
 }
