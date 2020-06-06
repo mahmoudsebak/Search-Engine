791b7d15672d6b6d412b86f29687d1311d05ce0c
Muhammad Sayed <muhammad.mahmoud98@eng-st.cu.edu.eg>
2020-06-06 05:41:22 +0200

    fix bug in personalized servlet

--- search/src/main/java/PersonalizedServlet.java
+++ search/src/main/java/PersonalizedServlet.java
@@ -5,12 +5,13 @@ import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/userAction")
 public class PersonalizedServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	protected void doGet(HttpServletRequest request)
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
         URL url = new URL(request.getParameter("url"));
         String BaseLink = url.getProtocol() + "://" + url.getHost();
